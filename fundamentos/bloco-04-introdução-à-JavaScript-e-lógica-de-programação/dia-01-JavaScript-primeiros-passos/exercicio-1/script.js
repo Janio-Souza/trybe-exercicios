cbaddfd71d14f3b729c85d7d08774ba1e67c806d
Janio_Souza <janiosouz001@gmail.com>
2022-06-22 22:29:10 -0300

    Criado arquivo de exercicios do dia

---
+++ fundamentos/bloco-04-introdução-à-JavaScript-e-lógica-de-programação/dia-01-JavaScript-primeiros-passos/exercicio-1/script.js
@@ -0,0 +1,22 @@
+let canditato = "Aprovado";
+
+switch (canditato) {
+    case "Aprovado":
+
+        console.log("Parabens!");
+        break;
+
+    case "Lista de Espera":
+
+        console.log("Aguarde por mais informações");
+        break;
+
+    case "Reprovado":
+
+        console.log("Tente novamente");
+        break;
+
+    default:
+    console.log("Não identificado");
+   
+}
